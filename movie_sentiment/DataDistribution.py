f5ee36778138281416bef7821298ce8caf9d0e19
dsyoon <dosangyoon@gmail.com>
2017-07-29 02:11:51 +0900

    data distribution 예제 주석 추가

--- movie_sentiment/DataDistribution.py
+++ movie_sentiment/DataDistribution.py
@@ -1,9 +1,11 @@
+# made by 도날드원
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # csv 전체 title
-Data_Title=['color', 'director_name', 'num_critic_for_reviews', 'duration',
+whole_Data_Title=['color', 'director_name', 'num_critic_for_reviews', 'duration',
             'director_facebook_likes', 'actor_3_facebook_likes', 'actor_2_name','actor_1_facebook_likes',
             'gross','genres', 'actor_1_name', 'movie_title', 'num_voted_users','cast_total_facebook_likes',
             'actor_3_name','facenumber_in_poster', 'plot_keywords', 'movie_imdb_link','num_user_for_reviews',
@@ -11,69 +13,80 @@ Data_Title=['color', 'director_name', 'num_critic_for_reviews', 'duration',
             'imdb_score	aspect_ratio','movie_facebook_likes']
 
 # 사용할 title
-Select_Title=['num_critic_for_reviews', 'duration','director_facebook_likes','actor_3_facebook_likes',
+data_Title=['num_critic_for_reviews', 'duration','director_facebook_likes','actor_3_facebook_likes',
               'actor_1_facebook_likes', 'gross','num_voted_users','cast_total_facebook_likes',
               'facenumber_in_poster','num_user_for_reviews','content_rating','budget','actor_2_facebook_likes','imdb_score',
               'aspect_ratio','movie_facebook_likes']
 
 # csv 읽기
-Movie_Data = pd.read_csv('resources/movie_metadata.csv')
+whole_Movie_Data = pd.read_csv('resources/movie_metadata.csv')
 # content rating의 값에 대해서 set
-index = list(set(Movie_Data['content_rating'].values.tolist()))
+index = list(set(whole_Movie_Data['content_rating'].values.tolist()))
 
+# content rating값들의 사전화
 # content rating의 set 값을 key로, 0부터 index를 값으로 만듬
 content_rating_index = {}
 for i, idx in enumerate(index):
     content_rating_index[idx] = i
 
+# content_rating_index를 기반으로 값을 재구성하기 위한 함수
 def setIdToValue(value):
     ret = content_rating_index[value]
     return ret
 
-Movie_Data['content_rating']=Movie_Data['content_rating'].apply(setIdToValue)
-Sel_MovieData=Movie_Data[Select_Title]
+# content_rating의 값을 index로 변경
+whole_Movie_Data['content_rating']=whole_Movie_Data['content_rating'].apply(setIdToValue)
 
-Movie_Data.head(1)
+# 사용할 title로만 다시 데이터를 만듬
+MovieData=whole_Movie_Data[data_Title]
 
+# 데이터 확인
+print(whole_Movie_Data.head())
+print(MovieData.head(1))
 
-Sel_MovieData.head(1)
-
-
+# Positive 데이터
 Positive_Th=5
 
-Pos_Data=Movie_Data[Sel_MovieData['imdb_score']>Positive_Th]
-Neg_Data=Movie_Data[Sel_MovieData['imdb_score']<=Positive_Th]
+# 전체 데이터 중에서 MovieData의 imdb_score가 Positive Threthold보다 큰 데이터만 확보
+Pos_Data=whole_Movie_Data[MovieData['imdb_score']>Positive_Th]
+# 전체 데이터 중에서 MovieData의 imdb_score가 Positive Threthold보다 작은 데이터만 확보
+Neg_Data=whole_Movie_Data[MovieData['imdb_score']<=Positive_Th]
 
-num_item=len(Select_Title)
+# 사용할 데이터의 필드 수
+num_item=len(data_Title)
 print(num_item)
 
-
-fig=plt.figure(figsize=(20,20))
-
-fig.add_subplot(2,2,1)
-Pos_Data[Select_Title[0]].plot(kind='kde')
-Neg_Data[Select_Title[0]].plot(kind='kde')
+# 1. # 첫 번째 컬럼 'num_critic_for_reviews'에 대한 그래프 (w, h - inch)
+fig=plt.figure(figsize=(5,5))
+# 2x2(행x열)의 그래프
+fig.add_subplot(1,1,1)
+# 그래프의 유형 (kind를 주지않으면 전체 데이터 출력, kde는 부드러운 선, bar는 막대 그래프
+Pos_Data[data_Title[0]].plot(kind='kde')
+Neg_Data[data_Title[0]].plot(kind='kde')
 plt.legend(['P','N'])
-plt.title(Select_Title[0])
+plt.title(data_Title[0])
+plt.show()
 
-
-
-fig=plt.figure(figsize=(20,20))
+# 2. 전체 컬럼에 대한 출력 그래프 (w, h - inch)
+fig=plt.figure(figsize=(10,10))
 num_col=2
 num_row= num_item/num_col
 if num_item%num_col:
     num_row+=1
 
+# num_row = 8: 8행 그래프를 생성하기 위함
+# num_col = 2: 2열 그래프를 생성하기 위함
 for i in range(num_item):
-    fig.add_subplot(num_row,num_col,i+1)
-    Pos_Data[Select_Title[i]].plot(kind='kde')
-    Neg_Data[Select_Title[i]].plot(kind='kde')
+    # (8*2)의 그래프
+    fig.add_subplot(num_row, num_col,i+1)
+    Pos_Data[data_Title[i]].plot(kind='kde')
+    Neg_Data[data_Title[i]].plot(kind='kde')
     plt.legend(['P','N'])
-    plt.title(Select_Title[i])
+    plt.title(data_Title[i])
+plt.show()
 
-
+# 3. sigmoid 함수 출력 그래프 (w, h - inch)
 plt.figure(figsize=(2,2))
-
 def sigmoid(t):
     return (1/(1+np.e**(-t)))
 
@@ -81,3 +94,4 @@ plot_range=np.arange(-6,6,0.1)
 
 y=sigmoid(plot_range)
 plt.plot(plot_range, y, color="red")
+plt.show()
